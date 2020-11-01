5a05b18cae6dde7826de40f8426b113672b62993
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-11-01 23:51:59 +0900

    develop

--- src/components/pages/Board/utils.ts
+++ src/components/pages/Board/utils.ts
@@ -2,24 +2,19 @@ import {Ticket} from '../../../types';
 
 const orderUnit = 65536;
 export function calcNewOrder(
-  originalTickets: Ticket[],
+  originalList: Ticket[],
   target: Ticket,
   newIndex: number
 ) {
-  const excludeSelfTickets = originalTickets.filter(
+  const excludeSelfTickets = originalList.filter(
     (_ticket) => _ticket.id !== target.id
   );
-  if (excludeSelfTickets.length === 0) {
-    return orderUnit;
-  } else if (newIndex <= 0) {
-    return excludeSelfTickets[0].order / 2.0;
-  } else if (newIndex >= originalTickets.length - 1) {
-    return originalTickets[excludeSelfTickets.length - 1].order + orderUnit;
-  } else {
-    return (
-      (excludeSelfTickets[newIndex - 1].order +
-        excludeSelfTickets[newIndex].order) /
-      2.0
-    );
-  }
+  const isSameList = excludeSelfTickets.length !== originalList.length;
+  const base = isSameList ? excludeSelfTickets : originalList;
+  const count = base.length;
+  if (count === 0) return orderUnit;
+  
+  const preOrder = base[newIndex - 1]?.order || 0;
+  const nextOrder = base[newIndex]?.order || base[count - 1].order + orderUnit;
+  return (preOrder + nextOrder) / 2.0;
 }
