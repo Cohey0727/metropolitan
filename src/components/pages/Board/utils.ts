4f4a7c3bca381db2f1ae2d88415c4fbf319f566e
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-11-02 00:30:26 +0900

    develop

--- src/components/pages/Board/utils.ts
+++ src/components/pages/Board/utils.ts
@@ -1,6 +1,7 @@
 import {Ticket} from '../../../types';
 
 const orderUnit = 65536;
+
 export function calcNewOrder(
   originalList: Ticket[],
   target: Ticket,
@@ -14,7 +15,7 @@ export function calcNewOrder(
   const count = base.length;
   if (count === 0) return orderUnit;
 
-  const preOrder = base[newIndex - 1]?.order || 0;
+  const prevOrder = base[newIndex - 1]?.order || 0;
   const nextOrder = base[newIndex]?.order || base[count - 1].order + orderUnit;
-  return (preOrder + nextOrder) / 2.0;
+  return (prevOrder + nextOrder) / 2.0;
 }
