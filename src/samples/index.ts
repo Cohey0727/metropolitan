030828629c9ec7b9a3a9bd72098646dfc218b035
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-11-13 16:31:15 +0900

    master

--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,8 +1,6 @@
-import tickets from './tickets.json';
 import users from './users.json';
 import projects from './projects.json';
 import {Project, Ticket, User} from '../types';
 
-export const ticketsData = tickets as Ticket[];
 export const usersData = users as User[];
 export const projectData = projects as Project[];
